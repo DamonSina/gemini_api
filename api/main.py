8ac2da8600baef6dabba0ff99a83e5065dfc7e7c
DamonSina <166271872+DamonSina@users.noreply.github.com>
2024-04-22 11:32:38 +0800

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -21,6 +21,7 @@ def gemini_chat(data: dict):
     history=data.get('history')
     if history is None:
         history=[]
+    print("history:",history)    
     try:
         # genai.configure(api_key=api_key,transport='rest')
         genai.configure(api_key=api_key)
@@ -28,33 +29,12 @@ def gemini_chat(data: dict):
         chat = model.start_chat(history=history)
         response = chat.send_message(prompt)
         text=response.text
-        response = {"content": text,"history":chat.history}
-        return response
+        response = {"content": text}
+        return response,
     except Exception as e:
         print("gemini_chat:",e)
         return None
 
-@app.post("/gemini/chat_v2", tags=["API"], summary="GEMINI")
-def gemini_chat_v2(data: dict):
-    api_key= data.get('api_key')
-    prompt_1 = data.get('prompt_1')
-    prompt_2 = data.get('prompt_2')
-    try:
-        # genai.configure(api_key=api_key,transport='rest')
-        genai.configure(api_key=api_key)
-        model = genai.GenerativeModel('gemini-pro')
-        chat = model.start_chat(history=[])
-        response_1 =chat.send_message(prompt_1)
-        text_1=response_1.text
-
-        response_2 =chat.send_message(prompt_2)
-        text_2=response_2.text
-
-        response = {"content_1": text_1,"content_2": text_2}
-        return response
-    except Exception as e:
-        print("gemini_chat:",e)
-        return None
     
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1",workers=1)

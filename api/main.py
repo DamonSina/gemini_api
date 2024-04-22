e6d9e331c195cd65bb9804748ed38bab5592e83f
DamonSina <166271872+DamonSina@users.noreply.github.com>
2024-04-22 10:59:57 +0800

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -34,5 +34,27 @@ def gemini_chat(data: dict):
         print("gemini_chat:",e)
         return None
 
+@app.post("/gemini/chat_v2", tags=["API"], summary="GEMINI")
+def gemini_chat_v2(data: dict):
+    api_key= data.get('api_key')
+    prompt_1 = data.get('prompt_1')
+    prompt_2 = data.get('prompt_2')
+    try:
+        # genai.configure(api_key=api_key,transport='rest')
+        genai.configure(api_key=api_key)
+        model = genai.GenerativeModel('gemini-pro')
+        chat = model.start_chat(history=[])
+        response_1 =chat.send_message(prompt_1)
+        text_1=response_1.text
+
+        response_2 =chat.send_message(prompt_2)
+        text_2=response_2.text
+
+        response = {"content_1": text_1,"content_2": text_2}
+        return response
+    except Exception as e:
+        print("gemini_chat:",e)
+        return None
+    
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1",workers=1)
